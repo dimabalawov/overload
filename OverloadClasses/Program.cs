5bec08ff2f3f713af2de1eb23cc2176324f1ac3c
dimabalawov <dimabalawov@gmail.com>
2024-10-28 19:32:00 +0200

    Добавьте файлы проекта.

---
+++ OverloadClasses/Program.cs
@@ -0,0 +1,8 @@
+﻿using JournalClass;
+using ShopClass;
+
+Journal myJournal = new Journal("My Adventure Journal", "Documenting my exciting adventures.", 2025, "380123456789", "traveler@example.com");
+myJournal.Print();
+
+Shop myShop = new Shop("Tech Haven", "Your go-to store for the latest gadgets.", "456 Elm St", "380987654321", "support@techhaven.com");
+myShop.Print();
